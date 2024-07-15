1da71742cf081f8b82aecedc12318007e2ba05cd
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2024-07-15 15:42:28 +0530

    Create a6.c

---
+++ a6.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<math.h>
+
+void main() {
+    int n;
+    printf("enter n=");
+    scanf("%d",&n);
+    int a = n * (3*n+1)/2;
+    printf("%d ", a);
+    
+}
