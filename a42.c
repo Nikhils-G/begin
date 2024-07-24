cfea1aca0e61becd71708aeea62a26ca6d084966
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2024-07-24 18:19:18 +0530

    Create a42.c
    ALERT ==== DONT BELIEVE IN THIS CODE !!! ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!ALERT ==== DONT BELIEVE IN THIS CODE !!!

---
+++ a42.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+     char str[]="";
+     int n=strlen(str);
+     int a=n-1;
+     if(a<=0)
+     {
+         printf("0");
+     }
+     else
+        printf("%d ",a);
+}
+
+
