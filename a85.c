bcd0b89859d090eb54cb45fa1a5ee2c8d5d32088
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2024-07-29 15:06:00 +0530

    Create a85.c

---
+++ a85.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void  main()
+{
+    int arr[]={1,2,3,4,5,9,10,11,12,13};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int k=8;
+    for(int i=k;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    for(int i=0;i<k;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
+
+
+
+## sometimes the question may be aske as tell me the appropiate form of
+code will coding such sorting form etc  ... 
+think about all such as aspects and understanf the codes and 
+implement the code more efficiecntly and neatly !!!
